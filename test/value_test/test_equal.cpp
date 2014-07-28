485d907eb4ff9cdcdf1c377b4bb711094d3688e2
octopus-prime <mike.gresens@gmail.com>
2014-07-28 22:04:56 +0200

    Tests for value.

--- test/value_test/test_equal.cpp
+++ test/value_test/test_equal.cpp
@@ -35,38 +35,62 @@ typedef mpl::map
 	mpl::pair<int_t, double_t>,
 	mpl::pair<int_t, date_t>,
 	mpl::pair<int_t, string_t>,
-	mpl::pair<int_t, binary_t>,
+	mpl::pair<int_t, binary_t>
+> equal_different_types_1;
 
+typedef mpl::map
+<
 	mpl::pair<long_t, double_t>,
-	mpl::pair<long_t, date_t>
-//	mpl::pair<long_t, string_t>,
-//	mpl::pair<long_t, binary_t>
+	mpl::pair<long_t, date_t>,
+	mpl::pair<long_t, string_t>,
+	mpl::pair<long_t, binary_t>,
 
-//	mpl::pair<double_t, date_t>,
-//	mpl::pair<double_t, string_t>
-//	mpl::pair<double_t, binary_t>
+	mpl::pair<double_t, date_t>,
+	mpl::pair<double_t, string_t>,
+	mpl::pair<double_t, binary_t>,
 
-//	mpl::pair<date_t, string_t>
-//	mpl::pair<date_t, binary_t>
+	mpl::pair<date_t, string_t>,
+	mpl::pair<date_t, binary_t>,
 
-//	mpl::pair<string_t, binary_t>
-> equal_different_types;
+	mpl::pair<string_t, binary_t>
+> equal_different_types_2;
+
+#define TEST_EQUAL(test_name, value1, value2)\
+BOOST_AUTO_TEST_CASE(test_name)\
+{\
+	BOOST_CHECK_EQUAL(equal_to()(value1, value1), true);\
+	BOOST_CHECK_EQUAL(equal_to()(value2, value2), true);\
+	BOOST_CHECK_EQUAL(equal_to()(value2, value1), false);\
+	BOOST_CHECK_EQUAL(equal_to()(value1, value2), false);\
+}
 
 BOOST_AUTO_TEST_SUITE(test_equal)
+
+const date_t date_1 = boost::posix_time::microsec_clock::universal_time();
+const date_t date_2 = boost::posix_time::microsec_clock::universal_time();
 
 BOOST_AUTO_TEST_CASE(equal_null)
 {
 	BOOST_CHECK_EQUAL(equal_to()(null_t(), null_t()), true);
 }
 
-BOOST_AUTO_TEST_CASE(equal_int)
+TEST_EQUAL(equal_boolean,	boolean_t(false), 	boolean_t(true))
+TEST_EQUAL(equal_int,		int_t(1), 			int_t(2))
+TEST_EQUAL(equal_long,		long_t(1), 			long_t(2))
+TEST_EQUAL(equal_double,	double_t(1),		double_t(2))
+TEST_EQUAL(equal_date,		date_1,				date_2)
+TEST_EQUAL(equal_string,	string_t("1"),		string_t("2"))
+TEST_EQUAL(equal_binary,	binary_t(1, 1),		binary_t(2, 1))
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(equal_different_1, T, equal_different_types_1)
 {
-	BOOST_CHECK_EQUAL(equal_to()(int_t(1), int_t(1)), true);
-	BOOST_CHECK_EQUAL(equal_to()(int_t(2), int_t(1)), false);
-	BOOST_CHECK_EQUAL(equal_to()(int_t(1), int_t(2)), false);
+	typedef typename T::first F;
+	typedef typename T::second S;
+	BOOST_CHECK_EQUAL(equal_to()(F(), S()), false);
+	BOOST_CHECK_EQUAL(equal_to()(S(), F()), false);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(equal_different, T, equal_different_types)
+BOOST_AUTO_TEST_CASE_TEMPLATE(equal_different_2, T, equal_different_types_2)
 {
 	typedef typename T::first F;
 	typedef typename T::second S;
