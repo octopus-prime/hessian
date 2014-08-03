83d8ce85a5733f4763b9e2b2d5abdf2305399e99
octopus-prime <mike.gresens@gmail.com>
2014-08-03 13:06:48 +0200

    Added generator tests for object.

---
+++ test/generator_test/test_object.cpp
@@ -0,0 +1,29 @@
+/*
+ * test_object.cpp
+ *
+ *  Created on: 18.11.2013
+ *      Author: mgresens
+ */
+
+#include "generator_helper.hpp"
+#include "../fixture/fixture_object.hpp"
+
+namespace hessian {
+namespace generator_test {
+
+#define TEST_OBJECT(CASE) TEST_CALL(test_generate_object_##CASE, object_##CASE, hessian_##CASE)
+
+BOOST_FIXTURE_TEST_SUITE(test_generate_object, fixture::fixture_object)
+
+TEST_OBJECT(0);
+TEST_OBJECT(1);
+TEST_OBJECT(2);
+TEST_OBJECT(2a);
+TEST_OBJECT(2b);
+// TODO: Skipping test_object_3 because of cycles.
+TEST_OBJECT(16);
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}
+}
