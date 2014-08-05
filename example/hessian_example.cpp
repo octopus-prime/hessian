ef4b92094c0117dcfe5dbfbd4addfd0754892cd3
octopus-prime <mike.gresens@gmail.com>
2014-08-05 09:54:54 +0200

    Improved example.

--- example/hessian_example.cpp
+++ example/hessian_example.cpp
@@ -12,14 +12,14 @@
 int
 main()
 {
-	const client::object_1 dto(0);
 	try
 	{
 		const client::service service = client::make_service("hessian.caucho.com", 80);
 		service->arg_int_0(0);
 		service->arg_double_0_0(0.0);
-		service->arg_object_1(dto);
-		service->reply_object_1();
+		service->arg_object_1(client::object_1(0));
+		const client::object_1 object_1 = service->reply_object_1();
+		std::cout << object_1.get_value() << std::endl;
 	}
 	catch (const std::exception& exception)
 	{
