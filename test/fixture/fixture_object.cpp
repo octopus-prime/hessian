361c7b6849d0ea6bf858d8f1e9bde0f56c52ecdf
octopus-prime <mike.gresens@gmail.com>
2014-08-07 22:47:36 +0200

    Generator tests for object.

--- test/fixture/fixture_object.cpp
+++ test/fixture/fixture_object.cpp
@@ -21,31 +21,59 @@ const list_t fixture_object::object_2a = list_of<object_t>(object_1)(object_1);
 const list_t fixture_object::object_2b = list_of<object_t>(object_1)(object_1);
 const list_t fixture_object::object_16 = list_of<object_t>()()()()()()()()()()()()()()()()();
 
-const string_t fixture_object::hessian_0 = string_t("C""\x1a""com.caucho.hessian.test.A0""\x90""\x60");
-const string_t fixture_object::hessian_1 = string_t("C""\x30""\x22""com.caucho.hessian.test.TestObject""\x91""\x06""_value""\x60""\x90");
-const string_t fixture_object::hessian_2 = string_t("\x7a""C""\x30""\x22""com.caucho.hessian.test.TestObject""\x91""\x06""_value""\x60""\x90""\x60""\x91");
-const string_t fixture_object::hessian_2a = string_t("\x7a""C""\x30""\x22""com.caucho.hessian.test.TestObject""\x91""\x06""_value""\x60""\x90""Q""\x91");
-const string_t fixture_object::hessian_2b = string_t("\x7a""C""\x30""\x22""com.caucho.hessian.test.TestObject""\x91""\x06""_value""\x60""\x90""\x60""\x90");
-const string_t fixture_object::hessian_16 = 	string_t
+//const string_t fixture_object::hessian_0 = string_t("C""\x1a""com.caucho.hessian.test.A0""\x90""\x60");
+//const string_t fixture_object::hessian_1 = string_t("C""\x30""\x22""com.caucho.hessian.test.TestObject""\x91""\x06""_value""\x60""\x90");
+//const string_t fixture_object::hessian_2 = string_t("\x7a""C""\x30""\x22""com.caucho.hessian.test.TestObject""\x91""\x06""_value""\x60""\x90""\x60""\x91");
+//const string_t fixture_object::hessian_2a = string_t("\x7a""C""\x30""\x22""com.caucho.hessian.test.TestObject""\x91""\x06""_value""\x60""\x90""Q""\x91");
+//const string_t fixture_object::hessian_2b = string_t("\x7a""C""\x30""\x22""com.caucho.hessian.test.TestObject""\x91""\x06""_value""\x60""\x90""\x60""\x90");
+//const string_t fixture_object::hessian_16 = string_t
+//(
+//	"X""\xa1"
+//	"C""\x1a""com.caucho.hessian.test.A0""\x90""\x60"
+//	"C""\x1a""com.caucho.hessian.test.A1""\x90""\x61"
+//	"C""\x1a""com.caucho.hessian.test.A2""\x90""\x62"
+//	"C""\x1a""com.caucho.hessian.test.A3""\x90""\x63"
+//	"C""\x1a""com.caucho.hessian.test.A4""\x90""\x64"
+//	"C""\x1a""com.caucho.hessian.test.A5""\x90""\x65"
+//	"C""\x1a""com.caucho.hessian.test.A6""\x90""\x66"
+//	"C""\x1a""com.caucho.hessian.test.A7""\x90""\x67"
+//	"C""\x1a""com.caucho.hessian.test.A8""\x90""\x68"
+//	"C""\x1a""com.caucho.hessian.test.A9""\x90""\x69"
+//	"C""\x1b""com.caucho.hessian.test.A10""\x90""\x6a"
+//	"C""\x1b""com.caucho.hessian.test.A11""\x90""\x6b"
+//	"C""\x1b""com.caucho.hessian.test.A12""\x90""\x6c"
+//	"C""\x1b""com.caucho.hessian.test.A13""\x90""\x6d"
+//	"C""\x1b""com.caucho.hessian.test.A14""\x90""\x6e"
+//	"C""\x1b""com.caucho.hessian.test.A15""\x90""\x6f"
+//	"C""\x1b""com.caucho.hessian.test.A16""\x90""O""\xa0"
+//);
+
+const string_t fixture_object::hessian_0 = string_t("C""\x00""\x90""\x60", 4);
+const string_t fixture_object::hessian_1 = string_t("C""\x00""\x91""\x06""_value""\x60""\x90", 12);
+const string_t fixture_object::hessian_2 = string_t("\x7a""C""\x00""\x91""\x06""_value""\x60""\x90""\x60""\x91", 15);
+const string_t fixture_object::hessian_2a = string_t("\x7a""C""\x00""\x91""\x06""_value""\x60""\x90""Q""\x91", 15);
+const string_t fixture_object::hessian_2b = string_t("\x7a""C""\x00""\x91""\x06""_value""\x60""\x90""\x60""\x90", 15);
+const string_t fixture_object::hessian_16 = string_t
 (
 	"X""\xa1"
-	"C""\x1a""com.caucho.hessian.test.A0""\x90""\x60"
-	"C""\x1a""com.caucho.hessian.test.A1""\x90""\x61"
-	"C""\x1a""com.caucho.hessian.test.A2""\x90""\x62"
-	"C""\x1a""com.caucho.hessian.test.A3""\x90""\x63"
-	"C""\x1a""com.caucho.hessian.test.A4""\x90""\x64"
-	"C""\x1a""com.caucho.hessian.test.A5""\x90""\x65"
-	"C""\x1a""com.caucho.hessian.test.A6""\x90""\x66"
-	"C""\x1a""com.caucho.hessian.test.A7""\x90""\x67"
-	"C""\x1a""com.caucho.hessian.test.A8""\x90""\x68"
-	"C""\x1a""com.caucho.hessian.test.A9""\x90""\x69"
-	"C""\x1b""com.caucho.hessian.test.A10""\x90""\x6a"
-	"C""\x1b""com.caucho.hessian.test.A11""\x90""\x6b"
-	"C""\x1b""com.caucho.hessian.test.A12""\x90""\x6c"
-	"C""\x1b""com.caucho.hessian.test.A13""\x90""\x6d"
-	"C""\x1b""com.caucho.hessian.test.A14""\x90""\x6e"
-	"C""\x1b""com.caucho.hessian.test.A15""\x90""\x6f"
-	"C""\x1b""com.caucho.hessian.test.A16""\x90""O""\xa0"
+	"C""\x00""\x90""\x60"
+	"C""\x00""\x90""\x61"
+	"C""\x00""\x90""\x62"
+	"C""\x00""\x90""\x63"
+	"C""\x00""\x90""\x64"
+	"C""\x00""\x90""\x65"
+	"C""\x00""\x90""\x66"
+	"C""\x00""\x90""\x67"
+	"C""\x00""\x90""\x68"
+	"C""\x00""\x90""\x69"
+	"C""\x00""\x90""\x6a"
+	"C""\x00""\x90""\x6b"
+	"C""\x00""\x90""\x6c"
+	"C""\x00""\x90""\x6d"
+	"C""\x00""\x90""\x6e"
+	"C""\x00""\x90""\x6f"
+	"C""\x00""\x90""O""\xa0",
+	71
 );
 
 }
